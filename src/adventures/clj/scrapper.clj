8ed59014e931d57d47c229a78eedb642ec00bf33
Amin Razavi <amin.razavi@gmail.com>
2021-04-22 07:17:27 +0430

    some code organization and a tab traversal

--- src/adventures/clj/scrapper.clj
+++ src/adventures/clj/scrapper.clj
@@ -10,6 +10,7 @@
 (def URL "https://www.irantalent.com/jobs/it-software-web-development-jobs?language=english&page=")
 (def Titles ["job descript" "requirement" "job categor" "employment type" "seniorit"])
 (def JobXPath "/html/body/app-root/div/div/div[2]/div/app-new-advanced-search/div/div/div[2]/div[1]/div/div[3]/div")
+(def IndustryXPath "/html/body/app-root/div/div/div[2]/div/app-new-advanced-search/div/div/div[2]/div[2]/app-job-preview/div[2]/mat-tab-group/div/mat-tab-body[2]/div/div/div[3]/div[1]/p[2]")
 (def LastPageXPath "/html/body/app-root/div/div/div[2]/div/app-new-advanced-search/div/div/div[2]/div[1]/div/div[3]/div[31]/it-pagination/div/div[1]/div/button[5]")
 
 (defn reindexof [v x]
@@ -17,7 +18,7 @@
     (keep-indexed #(if (some? %2) %1)
                   (mapv #(re-matches (re-pattern (str x ".*")) %) v))))
 
-(defn parser [text]
+(defn parser [text industry]
   (let [v        (st/split-lines (st/lower-case text))
         jobtitle (v 0)
         company  (v 1)
@@ -28,7 +29,8 @@
      (st/join (mapv v (range (inc (iv 1)) (iv 2)))) ; "requirements"
      (st/join (mapv v (range (inc (iv 2)) (iv 3)))) ; "job category"
      (st/join (mapv v (range (inc (iv 3)) (iv 4)))) ; "employment type"
-     (st/join (mapv v (range (inc (iv 4)) (count v))))]))
+     (st/join (mapv v (range (inc (iv 4)) (count v)))) ; Seniority
+     industry]))
 
 (defn gopage [driver pagenum]
   (do
@@ -36,12 +38,26 @@
     (wd/wait driver 3)
     (wd/wait-visible driver [{:id :selected-job-preview}])))
 
+(defn read-text [driver jobnum]
+  (do (wd/click driver (str JobXPath"["jobnum"]"))
+      (wd/wait driver 1)
+      (wd/wait-visible driver [{:id :selected-job-preview}])
+      (wd/get-element-text driver ".//*[@id='selected-job-preview']")))
+
+(defn read-industry [driver]
+  (do (wd/wait driver 0.5)
+      (wd/click driver ".//*[@id='mat-tab-label-0-1']")
+      (wd/wait-visible driver [{:id :mat-tab-content-0-1}])
+      (wd/get-element-text driver IndustryXPath)))
+
 (defn readjob [driver jobnum]
-  (do
-    (wd/click driver (str JobXPath"["jobnum"]"))
-    (wd/wait driver 1)
-    (wd/wait-visible driver [{:id :selected-job-preview}])
-    (parser (wd/get-element-text driver ".//*[@id='selected-job-preview']"))))
+  (let[text     (read-text driver jobnum)
+       industry (read-industry driver)]
+    (if (empty? text)
+      (parser (read-text driver jobnum) industry)
+      (if (empty? industry)
+        (parser text "NA")
+        (parser text industry)))))
 
 (defn job->csv [filename v]
   (with-open [writer (io/writer filename)]
